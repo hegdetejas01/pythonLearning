cc1fe7706810f9d5153288c4cdd6bedd2bbe961c
hegdetejas01 <hegde.tejas01@gmail.com>
2026-06-03 12:15:41 +0530

    end of session 4 - lists

---
+++ listComprehension.py
@@ -0,0 +1,106 @@
+############################
+###  LIST COMPREHENSION  ###
+############################
+
+
+# it is a consise way of creating list
+# newlist = [expression for items in iterable if condition = True]
+# more efficient and powerfull, requires fewer number of lines, interative statements into formulas
+
+
+# add 1 to 10 number in the list
+l = []
+for i in range(1,11):
+    l.append(i)
+print(l)
+
+# add 1 to 10 in list using list comprehension
+L = [i for i in range(1,11)]
+print(L)
+
+
+# scaler multiplication of a vector
+v = [2,3,4]
+s = -3
+x = []
+for i in v:
+    x.append(i*s)
+print(x)
+
+# scaler multiplication of a vector using list comprehension
+v = [2,3,4]
+s = -3
+result = [i*s for i in v]
+print(result)
+
+
+# Squares of the list
+n = [1,2,3,4,5]
+result = [i**2 for i in n]
+print(result)
+
+
+# all numbers divisible by 5 in range 1 to 50
+print([i for i in range(1,51) if i%5 == 0])
+
+
+# find languages that states with p
+languages = ['python','java','c','php','c++','ruby','prompt']
+result = [language for language in languages if language.startswith('p')]
+print(result)
+
+
+### Nested ifs with list comprehension
+
+# make new list from myfruit if it item is present in basket and also starts with 'a'
+basket = ['apple','cherry','almond','guava','banana']
+myfruits = ['apple','kivi','grapes','banana','almond']
+# print([fruit for fruit in myfruits if fruit in basket and fruit.startswith('a')])
+res = [fruit for fruit in myfruits if fruit in basket if fruit.startswith('a')]
+print(res)
+
+# print (3,3) matrix using list comprehension -> nested
+res = [[j for i in range(1,4)] for j in range (1,4)] # j is outer loop, i is inner loop
+print(res)
+
+print([[[i*j*k for i in range(1,3)] for j in range(1,3)] for k in range(1,3)]) # 3D matrix
+
+
+# Cartesian product
+l1 = [1,2,3,4]
+l2 = [5,6,7,8]
+res = [i*j for i in l1 for j in l2]
+print(res)
+
+
+
+
+
+
+
+####################################################
+################### ZIP FUNCTION ###################
+####################################################
+
+
+# it returns a zip object, which is an iterator of the tuple where the 1st item of each iterator are paired together, second item of each iterator is paired together and so on
+# if the lenght of the iterator is different, the the smallest iterator decides the lenght of the new iterator
+
+l1 = [1,2,3,4]
+l2 = [5,6,7,8]
+# l3 = zip(l1,l2)
+# l3 = list(l3)
+l3 = list(zip(l1,l2))
+print(l3)
+
+# Program to add the items of 2 lists indexwise
+l1 = [1,2,3,4]
+l2 = [5,6,7,8]
+# l3 = list(zip(l1,l2))
+# print([i+j for i,j in l3])
+print([i+j for i,j in zip(l1,l2)])
+
+
+l1 = [1,2,3] # this decides the lenght of the ziped list
+l2 = [1,2,3,4,5,6]
+print(list(zip(l1,l2)))
---
+++ lists.py
@@ -0,0 +1,214 @@
+# Python Lists
+
+# Basics:
+# 1. It is a datatype to store multiple items under one names
+# 2. Dynamic datatype
+# 3. Helps to handle large datas of same type
+
+# Differences between Array and Lists:
+# 1. Fixed versus Dynamic Size, i.e List is Dyanamic Array
+# 2. Arrays are homogeneous i.e only one data type can be stored, whereas lists are heterogeneous
+# 3. Speed of execution of List is slow compared to Arrays
+# 4. Lists occupy more space than Arrays
+
+# How lists are stored in memory?
+# 1. Arrays create continuous memory bloc
+# 2. Whereas lists are Referential Array, i.e, list stores address of the data - since the address is stored, this removes that only homogenous data has to be stored. But this demands more space and time.
+
+# Characteristic of list
+# 1. Lists are Ordered
+# 2. Lists are mutable unlike strings
+# 3. heterogeneous
+# 4. Items can be dulpicated
+# 5. Can be nested
+# 6. Dynamic
+# 7. Items can be accessed
+# 8. Can contain any kind of objects in list
+
+
+
+print([]) #empty list
+print([1,2,3]) #1D homogenous
+print([1,2,3,[4,6]]) #2D heterogeneous
+print([1,2,3.5,"Hello",5+8j]) #Heterogeneous
+print(list('hello'))
+
+
+
+l = [1,2,3,4,5,6,7]
+print(l[0])
+print(l[4])
+print(l[-1])
+print(l[-5])
+
+l = [1,2,3,[4,5]]
+print(l[3])
+print(l[3][0])
+print(l[3][-2])
+
+l = [[[1,2],[3,4]],[[5,6],[7,8]]]
+print(l[1])
+print(l[1][0][0])
+
+
+
+
+l = [1,2,3,4,5,6,7,8,9]
+print(l[:3])
+print(l[-3:])
+print(l[::-1]) # reversing the list
+print(l[0::2])
+
+
+
+
+l = [1,2,3,4]
+l.append(5) # append adds ONE value at the end of the list
+m = [1,2,3,4]
+m.extend([5,6,7]) # extend adds MULTIPLE value at the end of the list
+n = [1,2,3,4]
+n.append([6,7,8]) # it appends [6,7,8] as a list within n
+o = [1,2,3,4]
+o.extend('delhi') # it adds d e l h i into the list as saperate items
+p = [1,2,3,4]
+p.append('pune') # adds pune as a single item at the end of the list
+print(l,m,n,o,p,sep="\n")
+
+l = [1,2,3,4,5]
+l.insert(1,100) # add 100 at index 1
+print(l)
+
+
+
+
+l = [1,2,3,4,5]
+l[1] = 500
+print(l)
+
+l = [1,2,3,4,5,6]
+l[1:4] = [200,300]
+print(l)
+
+
+
+
+l = [1,2,3,4,5,6]
+del l
+# print(l) ----> This throws error because l doesn't exist anymore
+
+l = [1,2,3,4,5,6]
+del l[-1]
+print(l)
+
+l = [1,2,3,4,5,6,7]
+del l[2:5]
+print(l)
+
+l = [1,2,3,4,5,6,7]
+l.remove(3) # remove function works on values, i.e, here it removes the value 3 from the list
+print(l)
+# l.remove('x')   ----> This throws error since x is not in the list
+# print(l)
+
+l = [1,2,3,4,5,6,7]
+l.pop(0) # removes the value at 0th index
+l.pop() # if index is not mentioned, it removes the last item
+print(l)
+
+l = [1,2,3,4,5,6,7]
+l.clear() # it clears the list. l will become empty
+print(l)
+
+
+
+#### Operations on list
+
+#airthmatic - addition and multiplication
+l1 = [1,2,3,4]
+l2 = [5,6,7]
+print(l1+l2)
+print(l1*3)
+
+#membership operator
+l = [1,2,3,4,5,6,7]
+print(5 in l)
+print(5 not in l)
+
+l = [1,2,3,[4,5]]
+print(5 in l)
+print(5 in l[-1])
+print([4,5] in l)
+
+# Loops
+l1 = [1,2,3,4,5]
+l2 = [3,4,5,6,7,[8,9],0]
+for i in l1:
+    print(i)
+
+for i in l2:
+    print(i)
+
+l3 = [[[1,2],[3,4]],[[5,6],[7,8]]]
+for i in l3:
+    print(i)
+
+
+
+
+
+
+############ FUNCTIONS
+
+# Common functions - len, min, max, sorted
+
+l = [1,2,3,4,5,6,0]
+print(len(l))
+print(min(l)) # acts only for homogeneous data
+print(max(l))
+print(sorted(l))
+print(sorted(l,reverse=True))
+
+
+l = [1,2,3,1,2,3,4,5,1,1,1,1,1,1]
+print(l.count(1))
+print(l.count(0))
+
+print(l.index(1))
+print(l.index(3))
+# print(l.index('x')) # Error, because not present
+
+l.reverse() # permanently reverses the list
+print(l)
+
+# sorted in not a permanent change - temporory action
+# sort is permanent
+l = [1,3,1,8,3,10]
+print(l)
+print(sorted(l))
+print(l)
+l.sort()
+print(l)
+l.sort(reverse=True)
+
+# Copy creates a shallow copy i.e same list at a new address
+l = [1,2,3,4,5,6]
+print(id(l))
+m = l.copy()
+print(id(m))
+print(l,m)
+
+
+
+### Traversing a list
+# item wise traversing
+l = [1,2,3,4,5]
+for i in l:
+    print(i)
+
+# index based traversing
+l = [1,2,3,4,5]
+for i in range(0,len(l)):
+    print(l[i])
+
+
+    
