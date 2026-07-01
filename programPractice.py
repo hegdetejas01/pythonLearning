ecfd77ee6d6bf449c11b94efcb3ebc82121b4a67
rnagaratna <105634669+rnagaratna@users.noreply.github.com>
2026-07-01 22:08:29 +0530

    Update programPractice.py

--- programPractice.py
+++ programPractice.py
@@ -27,6 +27,19 @@ print("Sum is ", a+b+c)
 
 # find the minimim of 3 numbers
 
+num1 = int(input('Enter number 1: "))
+num2 = int(input('Enter number 2: "))
+num3 = int(input('Enter number 3: "))
+
+if (num1<num2) and (num1<num3):
+    print('smallest is',num1)
+elif num2<num3:
+    print('smallest is',num2)
+else:
+    print('smallest is',num3)
+    
+                 
+#ORR
 num1 = int(input("Enter number 1: "))
 num2 = int(input("Enter number 2: "))
 minimum = min(num1,num2)
