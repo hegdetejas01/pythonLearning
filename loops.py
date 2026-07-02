bf1b3c9d9b51a80c6a62a8b388e473af62d5bd5d
rnagaratna <105634669+rnagaratna@users.noreply.github.com>
2026-07-02 11:10:48 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -47,6 +47,11 @@ for i in "Delhi":
 n,m = int(input("enter num1: ")), int(input("enter num2: "))
 for i in range(1,n+1):
     for j in range(1,m+1):
+        print(i,j)
+
+n,m = int(input("enter num1: ")), int(input("enter num2: "))
+for i in range(1,n+1):
+    for j in range(1,m+1):
         if j>=i:
             print("(", i,", ", j, ")")
 
